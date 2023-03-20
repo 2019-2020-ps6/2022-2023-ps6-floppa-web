1e0f59293e15549adcbb5d15b4a77222191a5408
Arthur <arthur.rodriguez640@gmail.com>
2023-03-20 11:43:05 +0100

    screen to start the quiz #18

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { StartQuizComponent } from './quizzes/start-quiz/start-quiz.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
+    {path: 'start-quiz/:id', component: StartQuizComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
 ];
 
