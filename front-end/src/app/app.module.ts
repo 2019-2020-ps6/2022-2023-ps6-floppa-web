76c5113aa4a6aaf4a770b386ea0042c4d31823d8
Eloi <eloi.baudon@etu.unice.fr>
2023-03-20 12:05:36 +0100

    creation des fichiers

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { QuestionComponent } from './questions/question/question.component';
 import { UserComponent } from './users/user/user.component';
 import { UserFormComponent } from './users/user-form/user-form.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { HomeComponent} from './home-profiles/home/home.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { UserListComponent } from './users/user-list/user-list.component';
     UserComponent,
     UserFormComponent,
     UserListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
