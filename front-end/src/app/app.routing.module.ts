258f79030e85552428d5efd1c1efa851657644a9
Arthur <arthur.rodriguez640@gmail.com>
2023-03-23 21:13:47 +0100

    add hint #26

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -6,6 +6,7 @@ import { UserListComponent } from './users/user-list/user-list.component';
 import { StartQuizComponent } from './quizzes/start-quiz/start-quiz.component';
 import { PlayQuizComponent } from './quizzes/play-quiz/play-quiz.component';
 import { FinalScreenComponent } from './quizzes/final-screen/final-screen.component';
+import { PlayQuizComponent2 } from './quizzes/play-quiz-2/play-quiz-2.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
@@ -13,6 +14,7 @@ const routes: Routes = [
     {path: 'edit-quiz/:id', component: EditQuizComponent},
     {path: 'start-quiz/:id', component: StartQuizComponent},
     {path: 'play-quiz/:id', component: PlayQuizComponent},
+    {path: 'play-quiz-2/:id', component: PlayQuizComponent2},
     {path: 'final-screen/:id', component: FinalScreenComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
 ];
