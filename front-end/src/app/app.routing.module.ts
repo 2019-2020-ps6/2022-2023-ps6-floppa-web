d9c312db5192166e8c4cd5ee30687b30a816dc3e
Arthur <arthur.rodriguez640@gmail.com>
2023-03-25 16:44:34 +0100

    add sound aid #27

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -7,6 +7,7 @@ import { StartQuizComponent } from './quizzes/start-quiz/start-quiz.component';
 import { PlayQuizComponent } from './quizzes/play-quiz/play-quiz.component';
 import { FinalScreenComponent } from './quizzes/final-screen/final-screen.component';
 import { PlayQuizComponent2 } from './quizzes/play-quiz-2/play-quiz-2.component';
+import { PlayQuizComponent3 } from './quizzes/play-quiz-3/play-quiz-3.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
@@ -15,6 +16,7 @@ const routes: Routes = [
     {path: 'start-quiz/:id', component: StartQuizComponent},
     {path: 'play-quiz/:id', component: PlayQuizComponent},
     {path: 'play-quiz-2/:id', component: PlayQuizComponent2},
+    {path: 'play-quiz-3/:id', component: PlayQuizComponent3},
     {path: 'final-screen/:id', component: FinalScreenComponent},
     { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
 ];
