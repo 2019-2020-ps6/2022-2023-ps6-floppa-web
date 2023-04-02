be5675de5af72bbce2d98aaabdb1c2119d952910
Marcus Aas Jensen <aasjensenm@gmail.com>
2023-04-02 18:12:43 +0200

    feat: association line showing when creating associatoins in game. #13

--- front-end/src/app/quizzes/connection/connection.component.ts
+++ front-end/src/app/quizzes/connection/connection.component.ts
@@ -10,21 +10,58 @@ import { QUIZ_LIST } from 'src/mocks/quiz-list.mock';
 })
 export class ConnectionComponent implements OnInit {
 
+    @Input('connection')
+    connection: string[][] = [];
+
     @Input('id')
-    id: number;
+    id: number = 0;
+
+    @Input('shuffledValuesToConnect')
+    shuffledValuesToConnect: string[] = [];
+
+    @Input('shuffledValuesToBeConnected')
+    shuffledValuesToBeConnected: string[] = [];
 
     constructor() {
     }
 
     ngOnInit(): void {
-        this.changeLinePosition();
+        this.shuffledValuesToConnect = this.shuffledValuesToConnect.filter((value) => value != null);
+        this.shuffledValuesToBeConnected = this.shuffledValuesToBeConnected.filter((value) => value != null);
+        
+        let id1 = this.shuffledValuesToConnect.findIndex((value) => value == this.connection[0].toString());
+        let id2 = this.shuffledValuesToBeConnected.findIndex((value) => value == this.connection[1].toString());
+
+        this.createLine(id1,id2);
     }
 
-    changeLinePosition(): void {
-        let y2 = 100 * this.id;
+    createLine(i1: number, i2: number): void {
+
+        let y1 = 100 * i1;
+        let y2 = 100 * i2;
+        
         let line = document.getElementById("line" + this.id);
-        document.getElementById("line").setAttribute("y2", String(y2));
-        document.getElementById("line").setAttribute("x2", "300");
-        console.log(line);
+        
+        if(!line)
+        {
+            const svg = document.getElementsByTagName('svg')[0];
+            let g = document.createElement('line');
+            svg.appendChild(g);
+            g.setAttribute("x1","0"); 
+            g.setAttribute("y1","0"); 
+            g.setAttribute("x2","0"); 
+            g.setAttribute("y2","0");
+            g.setAttribute("stroke-width","10");
+            g.setAttribute("stroke","black");
+            g.setAttribute("id", "line" + this.id.toString());
+            line = g;
+        }
+
+        line.setAttribute("y2", String(y2));
+        line.setAttribute("y1", String(y1));
+
+        line.setAttribute("x2", "300");
+
+        console.log(document.getElementsByTagName('svg')[0]);
     }
 }
