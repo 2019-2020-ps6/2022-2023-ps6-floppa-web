355f1acd58ca745fa0eaf786b4a59ef9fc2b1dc9
Arthur <arthur.rodriguez640@gmail.com>
2023-04-29 13:14:38 +0200

    add progression calculation

--- front-end/src/app/caregiver/user-quiz-stats/user-quiz-stats.component.ts
+++ front-end/src/app/caregiver/user-quiz-stats/user-quiz-stats.component.ts
@@ -20,6 +20,7 @@ export class UserQuizStatsComponent implements OnInit {
     public lastGame: number;
     public meanScore: number;
     public quiz: Quiz;
+    public progression: number;
 
   constructor(private router: Router, private route: ActivatedRoute) {
     this.username = this.route.snapshot.paramMap.get("user");
@@ -44,18 +45,40 @@ export class UserQuizStatsComponent implements OnInit {
   getStats(): void {
     const today = new Date().getTime();
     let score = 0;
+    const scoreByDay: Record<number, number[]> = {};
+
+    const sortedQuizSessions = Object.keys(this.user.quizSessions) //croissant => date + récente en dernier
+      .sort((a,b) => this.user.quizSessions[a].date - this.user.quizSessions[b].date)
+      .reduce((acc,key) => {
+        acc[key] = this.user.quizSessions[key];
+        return acc;
+      },{});
+    this.user.quizSessions = sortedQuizSessions;
     this.lastGame = Number.MAX_VALUE;
     for (const session of Object.values(this.user.quizSessions)) {
       if (session.quizId === this.id) {
         score += this.computeScore(session.answers);
-        const diff = Math.abs(today - session.date) / (86400000);
+        const diff = Math.round(Math.abs(today - session.date) / (86400000));
         if (diff < this.lastGame) {
-            this.lastGame = Math.round(diff);
+            this.lastGame = diff;
+        }
+        if (scoreByDay[diff]) {
+          scoreByDay[diff].push(this.computeScore(session.answers));
+        }
+        else {
+          scoreByDay[diff] = [this.computeScore(session.answers)];
         }
         this.numberTry++;
       }
     }
     this.meanScore = score/this.numberTry;
+
+    const scoresByDayObj = {};
+    Object.keys(scoreByDay).forEach((diff) => {
+      const meanScore = scoreByDay[diff].reduce((acc, val) => acc + val, 0) / scoreByDay[diff].length;
+      scoresByDayObj[diff] = meanScore;
+    });
+    this.progression = this.computeProgression(scoresByDayObj);
   }
 
   computeScore(answers: boolean[]): number {
@@ -64,5 +87,32 @@ export class UserQuizStatsComponent implements OnInit {
       if (answer) score++;
     }
     return Math.round(score*100/answers.length);
+  }
+
+  computeProgression(scoresByDayObj: {}): number {
+    let sumOfScoreDiffs = 0;
+    let attempts = 0;
+    Object.values(scoresByDayObj).forEach((score,i,scoresArray) => {
+      if (i>0) {
+        sumOfScoreDiffs += Number(score) - Number(scoresArray[i-1]);
+        attempts++;
+      }
+    });
+    if (attempts > 0) {
+      return Math.round(sumOfScoreDiffs / attempts);
+    }
+    else {
+      return 0;
+    }
+  }
+
+  getProgressionClass(): string {
+    if (this.progression === 0) {
+      return '';
+    } else if (this.progression > 0) {
+      return 'positive';
+    } else {
+      return 'negative';
+    }
   }
 }
