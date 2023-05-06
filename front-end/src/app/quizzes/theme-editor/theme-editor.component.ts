4f8d1944fa81c57809a705ba4ae649c17294164d
Arthur <arthur.rodriguez640@gmail.com>
2023-05-06 13:30:57 +0200

    change theme creation with popup #67

--- front-end/src/app/quizzes/theme-editor/theme-editor.component.ts
+++ front-end/src/app/quizzes/theme-editor/theme-editor.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { THEME_QUIZ_LIST } from 'src/mocks/quiz-list.mock';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-theme-editor',
@@ -24,6 +25,48 @@ export class ThemeEditorComponent implements OnInit {
 
   goToTheme(themeIndex: number): void {
     this.router.navigate(["/quiz-editor/" + themeIndex]);
+  }
+
+  themeNotExists(themeTitle: string): boolean {
+    return THEME_QUIZ_LIST.map(theme => theme.title).find(title => title === themeTitle) !== undefined;
+  }
+  
+
+  createTheme(): void {
+    Swal.fire({
+      html: `<label for="title">
+        Thème :
+        <input class="input-text" type="text" id="title" placeholder="ex: géographie ">
+      </label>
+      <br>
+      <label for="description">
+        Description :
+        <input class="input-text" type="text" id="description">
+      </label>`,
+      confirmButtonText: 'Valider',
+      focusConfirm: false,
+      preConfirm: () => {
+        const titleInput = Swal.getPopup().querySelector('#title') as HTMLInputElement;
+        const descriptionInput = Swal.getPopup().querySelector('#description') as HTMLInputElement;
+        const title = titleInput.value;
+        const description = descriptionInput.value;
+        if (!title) {
+          Swal.showValidationMessage("Veuillez saisir un titre pour le thème")
+        }
+        else if (this.themeNotExists(title)) {
+          Swal.showValidationMessage("Ce thème existe déjà")
+        }
+        return { title: title, description: description}
+      }
+    }).then((result) => {
+      THEME_QUIZ_LIST.push({
+        id: THEME_QUIZ_LIST.length,
+        title: result.value.title,
+        description: result.value.description,
+        quizList: [],
+        coverImage: null
+      })
+    })
   }
 }
 
