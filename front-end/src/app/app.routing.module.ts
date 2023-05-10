115fe2872f2c6177479e8c30d18df53cb34f65e8
Eloi <eloi.baudon@etu.unice.fr>
2023-05-10 14:42:38 +0200

    page d'accueil

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { QuizListComponent } from './quizzes/quiz-list/quiz-list.component';
 import { EditQuizComponent } from './quizzes/edit-quiz/edit-quiz.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { HomeComponent} from './home-profiles/home/home.component';
 
 const routes: Routes = [
     {path: 'user-list', component: UserListComponent},
     {path: 'quiz-list', component: QuizListComponent},
     {path: 'edit-quiz/:id', component: EditQuizComponent},
-    { path: '', redirectTo: '/quiz-list', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
--- front-end/src/app/home-profiles/home/home.component.ts
+++ front-end/src/app/home-profiles/home/home.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
+    selector: 'app-home-profiles',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
-
-export class HomeComponent implements OnInit{
-    constructor(){}
+export class HomeComponent implements OnInit {
+    constructor(private router: Router) {}
     ngOnInit(): void {}
+    navigateToResidentConnection() {
+        this.router.navigate(['/resident-connection']);
+    }
+}
+
+// Récupère le bouton et la modale
+const btn = document.getElementById("staff-connection");
+const modal = document.getElementById("staffConnection");
+
+// Récupère l'élément <span> qui permet de fermer la modale
+const span = document.getElementsByClassName("close")[0] as HTMLElement;
+
+
+// Quand l'utilisateur clique sur le bouton, affiche la modale
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Quand l'utilisateur clique sur <span> (x), ferme la modale
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Quand l'utilisateur clique en dehors de la modale, ferme la modale
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
