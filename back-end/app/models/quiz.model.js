c7df85ca0d709f68139932aa0c548460c31f2860
ChloeVandenbrulle <chloe.vandenbrulle@etu.unice.fr>
2023-05-30 08:30:28 +0200

    task: modification quiz.model #99

--- back-end/app/models/quiz.model.js
+++ back-end/app/models/quiz.model.js
@@ -4,4 +4,7 @@ const BaseModel = require('../utils/base-model.js')
 module.exports = new BaseModel('Quiz', {
   theme: Joi.string().required(),
   name: Joi.string().required(),
+  questions: Joi.Question([]),
+  users: Joi.User([]),
+  coverImage: Joi.string(),
 })
