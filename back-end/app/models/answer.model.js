6547ef5507b2a1926f03671b17bdcd84fa1dce41
ChloeVandenbrulle <chloe.vandenbrulle@etu.unice.fr>
2023-05-30 08:55:29 +0200

    task: modification answer.model #99

--- back-end/app/models/answer.model.js
+++ back-end/app/models/answer.model.js
@@ -2,8 +2,7 @@ const Joi = require('joi')
 const BaseModel = require('../utils/base-model.js')
 
 module.exports = new BaseModel('Answer', {
-  type: Joi.string(),
   value: Joi.string().required(),
   isCorrect: Joi.boolean().required(),
-  questionId: Joi.number(),
+  img: Joi.string(),
 })
