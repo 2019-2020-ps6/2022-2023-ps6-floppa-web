772c88f53604b8d92f2a13c4b14447aa05c50d18
ChloeVandenbrulle <chloe.vandenbrulle@etu.unice.fr>
2023-06-16 14:47:42 +0200

    task: création compose + modifications dockerfile front pour variables d'environements

--- front-end/src/services/question.service.ts
+++ front-end/src/services/question.service.ts
@@ -10,7 +10,7 @@ import { environment } from 'src/environments/environment';
 })
 export class QuestionService {
   
-  private quizUrl = environment.apiUrl+"quizzes/";
+  private quizUrl = environment.apiUrl+"/quizzes/";
 
   constructor(private http: HttpClient) { }
 
