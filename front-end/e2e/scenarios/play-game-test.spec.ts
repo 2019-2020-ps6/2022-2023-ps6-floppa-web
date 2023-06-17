95340d92adac149341ec58da4607a1f15ab25972
ChloeVandenbrulle <chloe.vandenbrulle@etu.unice.fr>
2023-06-17 19:53:40 +0200

    test: test statistiques réglé

--- front-end/e2e/scenarios/play-game-test.spec.ts
+++ front-end/e2e/scenarios/play-game-test.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { testUrl } from 'e2e/e2e.config';
 
-test.describe('Play Game Valerie Pentacle', () => {
+test.describe('Play Game Valerie Pentacle and see stats', () => {
     test('Play Game Valerie Pentacle', async ({ page }) => {
         await page.goto('http://localhost:4200/home-profil-choice');
         await page.getByAltText('user-img-Valerie-Pentacle').click();
@@ -35,5 +35,47 @@ test.describe('Play Game Valerie Pentacle', () => {
 
         await page.getByRole('button', {name: 'Question Suivante'}).click();
         await expect(page.getByText('FÉLICITATIONS')).toBeVisible();
+    })
+    
+    test('See statisques after game',async ({page}) => {
+        await page.goto('http://localhost:4200/user-management');
+
+        await expect(page.getByTestId('add-button')).toBeVisible();
+        await expect(page.getByTestId('edit-button')).toBeVisible();
+
+        await expect(page.getByTestId('user-list')).toBeVisible();
+        await page.getByAltText('user-img-Valerie-Pentacle').click();
+        await expect(page.getByText('Voir les statistiques')).toBeVisible();
+        await expect(page.getByText('Valerie Pentacle')).toBeVisible();
+        await page.getByAltText('stats').click();
+        await expect(page.getByText('Valerie Pentacle')).toBeVisible();
+        await expect(page.getByAltText('user-img')).toBeVisible();
+        await page.getByRole('button', { name : 'Statistiques par quiz'}).click();
+        await expect(page).toHaveURL('http://localhost:4200/theme-list/Valerie-Pentacle/stats');
+        await expect(page.getByText('Choisissez un thème')).toBeVisible();
+        await page.getByAltText('theme-Les Animaux').click();
+        await expect(page.getByText('Valerie Pentacle')).toBeVisible();
+        await expect(page.getByAltText('user-img-Valerie-Pentacle')).toBeVisible();
+        await expect(page.getByText('Choisissez un quiz')).toBeVisible();
+        await page.getByAltText('quiz-img-Les félins').click();
+
+        await expect(page.getByAltText('user-img-Valerie-Pentacle')).toBeVisible();
+        await expect(page.getByAltText('quiz-img')).toBeVisible();
+
+        await page.getByText('Progression').click();
+        await expect(page.getByRole('dialog', {name: 'Progression'})).toBeVisible();
+        await page.getByRole('button', { name : 'OK'}).click();
+
+        await page.getByText('Similitude d\'erreur').click();
+        await expect(page.getByRole('dialog', {name: 'Similitude d\'erreur'})).toBeVisible();
+        await page.getByRole('button', { name : 'OK'}).click();
+
+        await page.getByText('voir questions problématiques').click();
+        await expect(page.getByRole('dialog', {name: 'Questions Problématiques'})).toBeVisible();
+        await page.getByRole('button', { name : 'OK'}).click();
+
+        await page.getByText('Temps moyen par question').click();
+        await expect(page.getByRole('dialog', {name: 'Temps moyen par question'})).toBeVisible();
+        await page.getByRole('button', { name : 'OK'}).click();        
     })
 })
